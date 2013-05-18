ee6c2bfef0498a1d31062228a192a0fee7607923
Brian Healey <brian.healey@gmail.com>
2013-05-18 15:47:52 -0700

    Updated the seeds file so that it can be ran multiple times (resets default datasets) Updated the seeds file to automatically create a default application with a default app_id and seed.  This will help sdk development as we don't have to repeatedled swap out our appid and seed in the config file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,28 @@
 
 #  Role.create(name: 'Prospective Developer')
 
-adminRole = Role.create!(:name => 'Admin')
-developerRole = Role.create!(:name => 'Developer')
-Role.create!(:name => 'Member')
-Role.create!(:name => 'Prospective Developer')
+adminRole = Role.find_or_create_by_name!('Admin')
 
-user = User.create!(:username => 'tokenfire_admin', :password => 'tokenfire', :password_confirmation => 'tokenfire')
+developerRole = Role.find_or_create_by_name!('Developer')
+
+Role.find_or_create_by_name!('Member')
+Role.find_or_create_by_name!('Prospective Developer')
+
+user = User.find_or_create_by_username!(:username => 'tokenfire_admin', :password => 'tokenfire', :password_confirmation => 'tokenfire')
 user.roles = [adminRole]
 
-user2 = User.create!(:username => 'tokenfire_developer', :password => 'tokenfire', :password_confirmation => 'tokenfire')
+user2 = User.find_or_create_by_username!(:username => 'tokenfire_developer', :password => 'tokenfire', :password_confirmation => 'tokenfire')
 user2.roles = [developerRole]
+
+# add default app so we don't have to keep swapping out our app id in the sdk sandbox app
+sandbox_app = user2.account.apps.find_or_create_by_name!( :name => 'tokenfiresandbox', :redirect_uri => 'urn:ietf:wg:oauth:2.0:oob',
+                            :description => 'TokenFire sandbox application for testing', :image => '')
+
+sandbox_app.uid = '6533f4c7beb3e2054984092401674aa2ea198d291c2e9e642c652209fe1cd829'
+sandbox_app.secret = 'b6df5911a6548a762623812910673749e39dcfecddc9afad2e00557ac0de186a'
+sandbox_app.save
+
+
+
+
+
